f592f47fdbd400e9bc0033ba2e6c1555ff81f3cf
Selectr0n <walcz.erik@gmail.com>
2021-05-09 15:44:41 +0200

    Batch run

--- nn_xray.py
+++ nn_xray.py
@@ -27,22 +27,22 @@ tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
 DATADIR = "data/"
 CATEGORIES = ["hand", "skull"]
 
-IMG_SIZE = 100
+IMG_SIZE = 20
 
 training_data = []
 
 def create_training_data():
-    for category in CATEGORIES:  # do dogs and cats
+    for category in CATEGORIES:
 
-        path = os.path.join(DATADIR,category)  # create path to dogs and cats
-        class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
+        path = os.path.join(DATADIR,category)
+        class_num = CATEGORIES.index(category)
 
-        for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
+        for img in tqdm(os.listdir(path)):
             try:
-                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
-                training_data.append([new_array, class_num])  # add this to our training_data
-            except Exception as e:  # in the interest in keeping the output clean...
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
                 pass
             
 create_training_data()
@@ -60,30 +60,39 @@ for features,label in training_data:
     X.append(features)
     y.append(label)
 
-#print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
-
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
-pickle_in = open("X.pickle","rb")
-X = pickle.load(pickle_in)
 
-pickle_in = open("y.pickle","rb")
-y = pickle.load(pickle_in)
+# =============================================================================
+# pickle_out = open("X.pickle","wb")
+# pickle.dump(X, pickle_out)
+# pickle_out.close()
+# 
+# pickle_out = open("y.pickle","wb")
+# pickle.dump(y, pickle_out)
+# pickle_out.close()
+# 
+# pickle_in = open("X.pickle","rb")
+# X = pickle.load(pickle_in)
+# 
+# pickle_in = open("y.pickle","rb")
+# y = pickle.load(pickle_in)
+# =============================================================================
 
 X=np.array(X/255.0)
 y=np.array(y)
 
 model = Sequential()
 
-model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Conv2D(20, (3, 3), input_shape=X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (3, 3)))
+model.add(Conv2D(20, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Flatten())
 
 model.add(Dense(64))
 model.add(Activation('relu'))
@@ -95,4 +104,4 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=20, validation_split=0.3, callbacks=[tensorboard])
+model.fit(X, y, batch_size=32, epochs=70, validation_split=0.3, callbacks=[tensorboard])
